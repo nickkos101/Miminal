c4b3767a21cb30783172903691fdcedbb038dfd6
Nicholas Koskowski <Nick@businessonmarketst.com>
2014-04-23 13:41:44 -0700

    Fixed Navigational issue

--- header.php
+++ header.php
@@ -12,17 +12,19 @@
 		<div class="container">
 			<img src="images/logo.png" class="logo" alt="logo">
 			<ul class="navigation">
-				<li><a href="http://businessonmarketst.com">Home</a></li>
-				<li><a href="http://businessonmarketst.com">Blog</a></li>
-				<li><a href="http://businessonmarketst.com">Services</a></li>
-				<li><a href="http://businessonmarketst.com">Contact</a></li>
+				<li><a href="/minimal/">Home</a></li>
+				<li><a href="/minimal/page.php">Page</a></li>
+				<li><a href="/minimal/blog.php">Blog</a></li>
+				<li><a href="/minimal/contact.php">Contact</a></li>
 				<li>Call  858-598-5477</li>
 			</ul>
 			<div class="mobile-menu">
 				<img src="images/nav-icon.png">
 				<ul>
-					<li><a href="http://businessonmarketst.com">Services</a></li>
-					<li><a href="http://businessonmarketst.com">Services</a></li>
+					<li><a href="#">Home</a></li>
+					<li><a href="minimal/page.php">Page</a></li>
+					<li><a href="/blog.php">Blog</a></li>
+					<li><a href="/contact.php">Contact</a></li>
 					<li>Call  858-598-5477</li>
 				</ul>
 			</div>
